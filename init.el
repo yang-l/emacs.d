3f92e98de7dccc79e160a7629ae22ce178127d8c
Y L <@>
2016-05-14 11:56:30 +1200

    compile init file

--- init.el
+++ init.el
@@ -6,4 +6,4 @@
 (setq load-prefer-newer t)                      ; always load newest byte code
 (require 'org)                                  ; init via org-mode
 (org-babel-load-file
-    (concat user-emacs-directory "README.org"))
+    (concat user-emacs-directory "README.org") t)
