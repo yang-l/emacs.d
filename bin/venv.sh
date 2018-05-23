c3b5c26f959d5fe51a69a5d856257f3f8d8113fd
YL <Y@L>
2018-05-23 16:27:23 +1200

    Defaulted to python3

--- bin/venv.sh
+++ bin/venv.sh
@@ -7,9 +7,9 @@ set -e
 
 if [ -d "$1" ]; then
     if [ ! -L "$1" ]; then
-        virtualenv "$1"
+        virtualenv -p $(which python3) "$1"
         source "${1}"/bin/activate
-        pip install -U pip
-        pip install -U -r ${2:-$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/requirements.txt}
+        pip3 install -U pip
+        pip3 install -U -r ${2:-$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/requirements.txt}
     fi
 fi
