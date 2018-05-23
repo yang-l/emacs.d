9fa7ad9bd7649da52cb7d334fbfd38485cfc6685
YL <Y@L>
2018-05-23 22:22:54 +1200

    Switched to jupyter for elpy

--- bin/venv.sh
+++ bin/venv.sh
@@ -9,7 +9,7 @@ if [ -d "$1" ]; then
     if [ ! -L "$1" ]; then
         virtualenv -p $(which python3) "$1"
         source "${1}"/bin/activate
-        pip3 install -U pip
-        pip3 install -U -r ${2:-$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/requirements.txt}
+        python3 -m pip install -U pip
+        python3 -m pip install -U -r ${2:-$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/requirements.txt}
     fi
 fi
