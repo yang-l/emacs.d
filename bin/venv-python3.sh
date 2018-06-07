21e4f274a91e639e2d1d87c62e1824cdf7c3bf4c
YL <Y@L>
2018-06-07 16:04:29 +1200

    Made Python virtualenv creation as a function

--- bin/venv-python3.sh
+++ bin/venv-python3.sh
@@ -11,5 +11,6 @@ if [ -d "$1" ]; then
         source "${1}"/bin/activate
         python3 -m pip install -U pip
         python3 -m pip install -U -r ${2:-$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/requirements.txt}
+        deactivate
     fi
 fi
