5e602feb70786a14f07af57b2e9255ae783329b3
YL <Y@L>
2018-06-08 16:39:42 +1200

    Added Bash LSP

---
+++ bin/venv-nodejs-bash.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+# $1 - virtualenv path
+# $2 - pip packages
+
+if [ -d "$1" ]; then
+    if [ ! -L "$1" ]; then
+        virtualenv -p $(which python) "$1"
+        source "${1}"/bin/activate
+        python -m pip install -U pip
+        python -m pip install -U -r ${2:-$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/requirements.txt}
+
+        ## Node.js configure
+        nodeenv -p
+        # /Applications/Python\ 3.6/Install\ Certificates.command
+        npm i -g bash-language-server
+
+        deactivate
+    fi
+fi
---
+++ lisp/lsp-bash.el
@@ -0,0 +1,5 @@
+(require 'lsp-mode)
+(lsp-define-stdio-client lsp-bash "bash"
+                         (lambda () default-directory)
+                         '("bash-language-server" "start"))
+(provide 'lsp-bash)
