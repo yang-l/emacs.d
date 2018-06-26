73cd06f44bfb96c9a4194c121bb50f56e5220bd1
YL <Y@L>
2018-06-27 09:30:32 +1200

    Changed the shared function in lsp venv creation

--- bin/venv-nodejs-npm.sh
+++ bin/venv-nodejs-npm.sh
@@ -4,18 +4,19 @@ set -e
 
 # $1 - virtualenv path
 # $2 - pip packages
+# $3 - npm packages
 
-if [ -d "$1" ]; then
-    if [ ! -L "$1" ]; then
-        virtualenv -p $(which python) "$1"
+if [ -d "${1}" ]; then
+    if [ ! -L "${1}" ]; then
+        virtualenv -p $(which python) "${1}"
         source "${1}"/bin/activate
         python -m pip install -U pip
-        python -m pip install -U -r ${2:-$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/requirements.txt}
+        python -m pip install -U -r "${2}"
 
         ## Node.js configure
         nodeenv -p
         # /Applications/Python\ 3.6/Install\ Certificates.command
-        npm i -g bash-language-server
+        npm i -g "${3}"
 
         deactivate
     fi
