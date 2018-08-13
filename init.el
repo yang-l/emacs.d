e271d961654ae3d994523284c3e8e779a90a9430
YL <Y@L>
2018-08-14 10:15:55 +1200

    Use https for all package source urls

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
 
 (require 'package)                              ; use-package bootstrap
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
