1cd7f255ef97443a14df7ab6894dd898cec2c2de
Y@L <Y@L>
2019-07-29 13:50:18 +1200

    Update python virtualenv setup in lsp mode

--- bin/venv-nodejs-npm.sh
+++ bin/venv-nodejs-npm.sh
@@ -3,21 +3,22 @@
 set -ex
 
 # $1 - virtualenv path
-# $2 - pip packages
-# $3 - node.js version
-# $4 - npm packages - comma separated package string
+# $2 - python version
+# $3 - pip packages file
+# $4 - node.js version
+# $5 - npm packages - comma separated package string
 
 if [ -d "${1}" ]; then
     if [ ! -L "${1}" ]; then
-        virtualenv -p $(which python) "${1}"
+        virtualenv -p $(which ${2}) "${1}"
         source "${1}"/bin/activate
-        python -m pip install -U pip
-        python -m pip install -U -r "${2}"
+        ${2} -m pip install -U pip
+        ${2} -m pip install -U -r "${3}"
 
         ## Node.js configure
-        nodeenv -p --node=${3}
+        nodeenv -p --node=${4}
         # /Applications/Python\ 3.6/Install\ Certificates.command
-        for APP in $(echo ${4} | sed 's/,/ /g')
+        for APP in $(echo ${5} | sed 's/,/ /g')
         do
             npm i -g "${APP}"
         done
--- bin/venv-python3.sh
+++ bin/venv-python3.sh
@@ -3,14 +3,15 @@
 set -ex
 
 # $1 - virtualenv path
-# $2 - pip packages
+# $2 - python version
+# $3 - pip packages
 
 if [ -d "${1}" ]; then
     if [ ! -L "${1}" ]; then
-        virtualenv -p $(which python3) "${1}"
+        virtualenv -p $(which ${2}) "${1}"
         source "${1}"/bin/activate
-        python3 -m pip install -U pip
-        python3 -m pip install -U -r "${2}"
+        ${2} -m pip install -U pip
+        ${2} -m pip install -U -r "${3}"
         deactivate
     fi
 fi
