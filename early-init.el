17235f84c6b8757bc1f9dc397c3bce2c74621da6
Y@L <Y@L>
2021-11-01 09:01:58 +1300

    Performance tuning

--- early-init.el
+++ early-init.el
@@ -15,6 +15,16 @@
       menu-bar-mode   nil
       scroll-bar-mode nil)
 
+(unless (or (daemonp) noninteractive)
+  (setq vc-follow-symlinks t)                   ; prevent emacs to crash when opening a symbolic link
+  (setq-default inhibit-redisplay t
+                inhibit-message t)
+  (add-hook 'window-setup-hook
+            (lambda ()
+              (setq-default inhibit-redisplay nil
+                            inhibit-message nil)
+              (redisplay))))
+
 ; disable 'Package cl is deprecated' message
 (setq byte-compile-warnings '(not obsolete))
 
