d0484e1188aa9f609a1967647a2ff8f6ce2bbc14
Y@L <Y@L>
2022-01-09 22:55:29 +1300

    use-package format updates

--- early-init.el
+++ early-init.el
@@ -10,6 +10,11 @@
 (setq load-prefer-newer t)
 (setq package-enable-at-startup nil)            ; disable package.el
 
+(when (and (fboundp 'native-comp-available)
+        (native-comp-available))
+  (when (boundp 'comp-deferred-compilation)
+    (setq comp-deferred-compilation t)))        ; native-comp
+
 ; disable at the earliest stage
 (setq tool-bar-mode   nil
       menu-bar-mode   nil
