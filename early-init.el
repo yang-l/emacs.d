c6ef8c5ccb7cf78548a0495e7621e9a1e8bd4f5f
Y@L <Y@L>
2025-06-24 19:05:39 +1200

    Some performance tuning

--- early-init.el
+++ early-init.el
@@ -5,7 +5,10 @@
 ;;; Code:
 
 ;; optimise startup speed
-(setq gc-cons-threshold most-positive-fixnum)   ; set GC at beginning to speed up Emacs loading
+(setq gc-cons-threshold most-positive-fixnum    ; set GC at beginning to speed up Emacs loading
+      gc-cons-percentage 0.6
+      read-process-output-max (* 16 1024 1024)
+      process-adaptive-read-buffering nil)
 
 (setq load-prefer-newer nil                     ; all default values
       native-comp-deferred-compilation t
@@ -19,6 +22,10 @@
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
 
 (setq frame-resize-pixelwise t)                 ; ensure turly full-screen
 
